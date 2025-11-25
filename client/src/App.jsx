14b6008446a5735db1962efa385392c536cc7299
Mohanlal Rana <mr7569107@gmail.com>
2025-11-25 14:41:19 +0545

    set routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+// import Signup from './pages/Signup'
 function App() {
 
   return (
-    <>
-    <h1>hello react</h1>
-    </>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      {/* <Route path='/signup' element={<Signup/>}/> */}
+     </Routes>
+     </BrowserRouter>
   )
 }
 
