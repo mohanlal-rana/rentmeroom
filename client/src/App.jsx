54a1616162107362166180f83e6fc8da70baeb06
Mohanlal Rana <mr7569107@gmail.com>
2025-11-26 11:44:59 +0545

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
      <BrowserRouter>
       <Header/>
+      
      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/signup' element={<Signup/>}/>
